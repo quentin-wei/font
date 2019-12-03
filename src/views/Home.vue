d7ec9a9ce9a6385ff9b85391d8d4bc35f2e5d347
weiguitie@163.com <weiguitie@163.com>
2019-12-03 18:47:05 +0800

    ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-          <p>hello world</p>
+          <p>is ok? yes</p>
           <ul>
             <li v-for="(item,index) in users" :key="index">
               {{item.id}} --:---{{item.name}}
