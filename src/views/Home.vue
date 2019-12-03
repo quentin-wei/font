57574d97231add3ee7e27c313b624b2e39520ba1
weiguitie@163.com <weiguitie@163.com>
2019-12-03 14:36:42 +0800

    font code

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+          最后一次整体测试修改前端代码
+          <ul>
+            <li v-for="(item,index) in users" :key="index">
+              {{item.id}} --:---{{item.name}}
+            </li>
+          </ul>
+          阿斯达四大所
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'home',
+ data(){
+   return{
+     users:[]
+   }
+ },
+ methods:{
+
+ },
+ mounted(){
+   axios.get('/api/users')
+   .then(res=>{
+     this.users = res.data
+   })
+ }
+}
+</script>
