dd8116f48f9c5603f1ea28ac06112ad58f599b78
weiguitie@163.com <weiguitie@163.com>
2019-12-03 18:27:18 +0800

    cg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-          最后一次整体测试修改前端代码
+          <p>hello world</p>
           <ul>
             <li v-for="(item,index) in users" :key="index">
               {{item.id}} --:---{{item.name}}
