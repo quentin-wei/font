6d7ea3f4bdeecd04185e0cbf6fe72148fa117df2
quentin wei <massbager@gmail.com>
2021-07-31 18:26:19 +0800

    new test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
     >
       {{ item.name }}
     </div>
+    <p>hello wep</p>
   </div>
 </template>
 
@@ -15,22 +16,22 @@ export default {
   data() {
     return {
       list: [
-        { name: 'vue-cli4脚手架' }
-        // { name: 'vant按需引入' },
-        // { name: '移动端rem适配' },
-        // { name: 'axios拦截封装' },
-        // { name: 'util工具类函数封装' },
-        // { name: 'vue-router配置' },
-        // { name: '登录权限校验' },
-        // { name: '多环境变量配置' },
-        // { name: 'vue.config.js配置' },
-        // { name: 'toast组件封装' },
-        // { name: 'dialog组件封装' },
-        // { name: '跨域代理设置' },
-        // { name: 'webpack打包可视化分析' },
-        // { name: 'CDN资源优化' },
-        // { name: 'gzip打包优化' },
-        // { name: '首页添加骨架屏' }
+        { name: 'vue-cli4脚手架' },
+        { name: 'vant按需引入' },
+        { name: '移动端rem适配' },
+        { name: 'axios拦截封装' },
+        { name: 'util工具类函数封装' },
+        { name: 'vue-router配置' },
+        { name: '登录权限校验' },
+        { name: '多环境变量配置' },
+        { name: 'vue.config.js配置' },
+        { name: 'toast组件封装' },
+        { name: 'dialog组件封装' },
+        { name: '跨域代理设置' },
+        { name: 'webpack打包可视化分析' },
+        { name: 'CDN资源优化' },
+        { name: 'gzip打包优化' },
+        { name: '首页添加骨架屏' }
       ]
     }
   },
